abaae6cab56b3ac53640a4a21885c1af684be314
quintin-lee <2449164582@qq.com>
2024-11-13 17:20:14 +0800

    添加 archlinux/manjaro pkg 包打包脚本

---
+++ tools/make_pkg.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+SCRIPT_DIR=$(dirname "$(realpath "$0")")/..
+
+PENV_TAR=${SCRIPT_DIR}/penv.tar.gz
+
+# create penv.tar.gz
+[ -f ${PENV_TAR} ] && rm -f ${PENV_TAR}
+
+(cd $SCRIPT_DIR; tar zcvf ${PENV_TAR} penv scripts)
+
+# generate pkg
+makepkg -f
